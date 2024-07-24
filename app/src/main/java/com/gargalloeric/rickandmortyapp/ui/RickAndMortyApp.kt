ce0517625d3b04336f64fbcb0da88ff805509276
gargalloeric <erickmontes555@gmail.com>
2024-07-24 20:36:42 +0200

    Add list screen to app composable

--- app/src/main/java/com/gargalloeric/rickandmortyapp/ui/RickAndMortyApp.kt
+++ app/src/main/java/com/gargalloeric/rickandmortyapp/ui/RickAndMortyApp.kt
@@ -1,13 +1,17 @@
 package com.gargalloeric.rickandmortyapp.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.gargalloeric.rickandmortyapp.ui.screen.character.list.CharacterListScreen
+import com.gargalloeric.rickandmortyapp.ui.screen.character.list.CharacterListViewModel
 
 enum class Screen {
     CharacterList
@@ -24,7 +28,11 @@ fun RickAndMoryApp(
             startDestination = Screen.CharacterList.name
         ) {
             composable(route = Screen.CharacterList.name) {
-                TODO("Add character list screen composable")
+                val characterListViewModel: CharacterListViewModel = viewModel(factory = CharacterListViewModel.Factory)
+                CharacterListScreen(
+                    modifier = Modifier.fillMaxSize(),
+                    pagingDataFlow = characterListViewModel.getCharacters()
+                )
             }
         }
     }
