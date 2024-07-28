e73343ce236da5630979871d963c418dc511bff5
gargalloeric <erickmontes555@gmail.com>
2024-07-28 19:21:48 +0200

    Add app top bar

--- app/src/main/java/com/gargalloeric/rickandmortyapp/ui/RickAndMortyApp.kt
+++ app/src/main/java/com/gargalloeric/rickandmortyapp/ui/RickAndMortyApp.kt
@@ -1,15 +1,28 @@
 package com.gargalloeric.rickandmortyapp.ui
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.gargalloeric.rickandmortyapp.R
 import com.gargalloeric.rickandmortyapp.ui.screen.character.list.CharacterListScreen
 import com.gargalloeric.rickandmortyapp.ui.screen.character.list.CharacterListViewModel
 
@@ -17,11 +30,34 @@ enum class Screen {
     CharacterList
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RickAndMoryApp(
     navController: NavHostController = rememberNavController()
 ) {
-    Scaffold { innerPadding ->
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    val appIcon = painterResource(id = R.drawable.icons8_rick_sanchez)
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            painter = appIcon,
+                            contentDescription = null
+                        )
+                        Spacer(modifier = Modifier.width(4.dp))
+                        Text(
+                            text = "Rick And Morty",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 25.sp
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
         NavHost(
             modifier = Modifier.padding(innerPadding),
             navController = navController,
